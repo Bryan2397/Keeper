e8b89fa1dc1c14b8f7008f744327e312ab83c961
Bryan Mirtil <166929786+Bryan2397@users.noreply.github.com>
2025-10-31 23:49:45 -0400

    Delete keeper-app-part-2-starting-forked directory

--- keeper-app-part-2-starting-forked/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import "./../public/styles.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-
-//Challenge. Render all the notes inside notes.js as a seperate Note
-//component.
